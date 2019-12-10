14f8c77013176545cf6324197ce5272ccc296fc2
Summer Rizzo <srainrizzo@gmail.com>
2019-12-10 10:14:51 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+#Create dog class
+class Dog
+    #woof
+end
+
+#Create three instances of Dogs named fido, snoopy, and lassie.
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+#Create Person class
+class Person
+    #probably bad
+end
+
+#Create two instances of PErson class named adele_goldberg and alan_kay
+adele_goldberg = Person.new
+alan_kay = Person.new
